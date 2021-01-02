d44a2351e868e20c416fff37418cf627a21b919b
kiranshetty <kiran.shetty@aa.com>
2021-01-01 20:06:51 -0600

    getting data from Firestore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Video from "./Video";
+import db from "./firebase";
 import './App.css';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    db.collection('videos').onSnapshot(snapshot => (
+      setVideos(snapshot.docs.map(doc => doc.data()))
+    ))
+  }, [])
+
   return (
     <div className="app">
        <div className="app__videos">
-          <Video
-          url="https://v21-us.tiktokcdn.com/video/tos/useast2a/tos-useast2a-ve-0068c001/4191252e8ae841ad815a151480dbfac4/?VExpiration=1609241406&VSignature=d5dfd216897f2bf7df89e38074c058eb&a=1233&br=3070&bt=1535&cd=0%7C0%7C1&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202012290529510101902192232D034DDF&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=anVtdDdmOnJkeDMzNjczM0ApPDloNzRlM2VkNzo7NGk1O2czYzU1NmVjYy5fLS0yMTZzczNfLjY1MjIxYWEzNC9hYWI6Yw%3D%3D&vl=&vr=" 
-          channel="Raymok"
-          description="kiddos dancing"
-          song="My shot- hamilton"
-          likes={300}
-          messages={135}
-          shares={20}
-          />
-          <Video />
-          <Video />
-          <Video />
+         {videos.map(({ url, channel, description, song, likes, messages, shares}) => (
+            <Video 
+            url={url}
+            channel={channel}
+            song={song}
+            description={description}
+            likes={likes}
+            messages={messages}
+            shares={shares}
+            />
+           )
+         )}
        </div>
     </div>
   );
