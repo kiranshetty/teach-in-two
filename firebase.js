1c9a2482585258175a74865ac155e9f6fd0709d4
kiranshetty <kiran.shetty@aa.com>
2021-01-01 19:21:48 -0600

    firebase config added

--- firebase.js
+++ firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAhUEH9YQIiYwH6CsPPhWnwFndEqDKTL64",
     authDomain: "teach-in-two.firebaseapp.com",
@@ -8,3 +10,9 @@ const firebaseConfig = {
     appId: "1:316870296296:web:6ebbe17459e4a9b9d3ff5d",
     measurementId: "G-W1Z1JZM88B"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+
+  export default db;
--- src/Video.js
+++ src/Video.js
@@ -1,8 +1,10 @@
 import React, {useRef, useState} from 'react'
 import "./Video.css"
 import VideoFooter from "./VideoFooter"
+import VideoSidebar from "./VideoSidebar"
 
-function Video() {
+function Video({ url, channel, description, song, likes, messages, shares}) 
+{
     const[playing, setPlaying] = useState(false);
     const videoRef = useRef(null);
 
@@ -23,9 +25,13 @@ function Video() {
                 loop 
                 onClick={onVideoPress}
                 ref={videoRef}
-                src="https://v21-us.tiktokcdn.com/video/tos/useast2a/tos-useast2a-ve-0068c001/4191252e8ae841ad815a151480dbfac4/?VExpiration=1609241406&VSignature=d5dfd216897f2bf7df89e38074c058eb&a=1233&br=3070&bt=1535&cd=0%7C0%7C1&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202012290529510101902192232D034DDF&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=anVtdDdmOnJkeDMzNjczM0ApPDloNzRlM2VkNzo7NGk1O2czYzU1NmVjYy5fLS0yMTZzczNfLjY1MjIxYWEzNC9hYWI6Yw%3D%3D&vl=&vr=">
+                src={url}>
             </video>
-            <VideoFooter/>
+            <VideoFooter
+              channel={channel}
+              description={description}
+              song={song}/>
+            <VideoSidebar likes={likes} shares={shares} messages={messages}/>  
         </div>
     )
 }
